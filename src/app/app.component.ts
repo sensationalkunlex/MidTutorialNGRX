44b41d153b83eac985c3df860fa8ff0bc3ffb201
Oyekunle Oyewusi <sensationalkunlex@gmail.com>
2022-09-21 06:33:04 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { GoogleBooksService } from './book-list/books.service';
+import { addBook, removeBook, retrievedBookList } from './state/books.actions';
+import { selectBookCollection, selectBooks } from './state/books.selectors';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BookNGRX';
+  books$ = this.store.select(selectBooks) ;
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  onAdd(bookId: string) {
+    this.store.dispatch(addBook({ bookId }));
+  }
+
+  onRemove(bookId: string) {
+    this.store.dispatch(removeBook({ bookId }));
+  }
+
+  constructor(
+    private booksService: GoogleBooksService,
+    private store: Store
+  ) {}
+
+  ngOnInit() {
+    this.booksService
+      .getBooks()
+      .subscribe((books) => this.store.dispatch(retrievedBookList({ books })));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookCollectionComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
